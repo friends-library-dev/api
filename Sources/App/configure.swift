cfca03338db535bbe179d90e782a0ddfc4b6079c
Jared Henderson <jared@netrivet.com>
2021-09-08 16:51:39 -0400

    add migration+model for Friend

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -44,6 +44,7 @@ private func addMigrations(to app: Application) {
   app.migrations.add(CreateOrderItems())
   app.migrations.add(CreateTokens())
   app.migrations.add(CreateTokenScopes())
+  app.migrations.add(CreateFriends())
 
   if Environment.get("SEED_DB") == "true" || app.environment == .testing {
     app.migrations.add(Seed())
